c5608a67ac2d12a07db5b58d96320af6e8513bbd
Oleksandr Diudiun <a.diudiun@gmail.com>
2024-03-04 22:38:43 +0000

    jupiter

--- week1/Model.py
+++ week1/Model.py
@@ -1,1 +1,5 @@
 # Class for Model and all functions implementation
+# Weights amount function based on hidden layer neurons amount and inpud vector size
+# random weights for fist time matrix amount of INput X amount of nodes in hiden layer
+# random Bias Vector
+#
